26164f6522e2dadc4b3d0ec1bf916f24b245ee16
sweta <swetat007@protonmail.com>
2022-12-12 05:30:48 +0100

    Added guest list txt file

---
+++ read.py
@@ -0,0 +1,16 @@
+# file = open("filehendling.txt")
+
+# for line in file :
+#     print(line + " *new*")
+
+# file.close()
+
+
+file = open("guest_list.txt")
+
+for guest in file :
+    print(guest)
+
+file.close()
+
+
